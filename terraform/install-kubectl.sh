7669c9b0b84198db5559728731bda01726126bc4
John Studarus <john@jhlconsulting.com>
2019-03-27 21:55:13 +0000

    suppress warning

--- terraform/install-kubectl.sh
+++ terraform/install-kubectl.sh
@@ -1,5 +1,8 @@
 
 
+# suppress apt-key warning message
+APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
+
 sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
