d09b151994a206cbc9dc67ccfd193622bbdd62d7
John Studarus <john@jhlconsulting.com>
2019-03-28 03:29:55 +0000

    let end user start up minikube

--- terraform/install-minikube.sh
+++ terraform/install-minikube.sh
@@ -2,9 +2,3 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 chmod +x minikube
 cp minikube /usr/local/bin
 rm minikube
-
-minikube start  --vm-driver kvm2  --cpus 4  --memory 6144  --feature-gates=RuntimeClass=true  \
-	--network-plugin=cni  --enable-default-cni  --container-runtime=cri-o  --bootstrapper=kubeadm
-minikube stop
-
-
