d5becebfe9da20e26dccc045b8e02fd74cb6b701
Bharat Kunwar <b.kunwar@gmail.com>
2019-04-30 17:12:41 +0100

    Output SSH instructions and Ansible inventory

--- output.tf
+++ output.tf
@@ -7,9 +7,18 @@ output "Alternative Registry IP" {
 }
 
 output "Lab IPs" {
-  value = "${packet_device.lab.*.access_public_ipv4}"
+  value = "${
+    formatlist(
+      "ssh -o PreferredAuthentications=password lab@%s",
+      concat(packet_device.lab_alt.*.access_public_ipv4, packet_device.lab.*.access_public_ipv4)
+  )}"
 }
 
-output "Alternative Lab IPs" {
-  value = "${packet_device.lab_alt.*.access_public_ipv4}"
+output "Ansible Inventory" {
+  value = "${
+    join("", formatlist(
+      "\n%s ansible_host=%s ansible_user=root",
+      concat(packet_device.lab_alt.*.hostname, packet_device.lab.*.hostname),
+      concat(packet_device.lab_alt.*.access_public_ipv4, packet_device.lab.*.access_public_ipv4)
+  ))}"
 }
