4dcbcae3262c165fec68eed452d20ab67773e243
Bharat Kunwar <brtknr@bath.edu>
2020-04-17 14:01:32 +0100

    Improve output

--- output.tf
+++ output.tf
@@ -1,24 +1,27 @@
-output "Registry-IP" {
+output "registry_ip" {
   value = "${packet_device.registry.access_public_ipv4}"
 }
 
-output "Alternative-Registry-IP" {
+output "registry_ip_alt" {
   value = "${packet_device.registry_alt.access_public_ipv4}"
 }
 
-output "Lab-IPs" {
+output "lab_ips" {
   value = "${
-    join("", formatlist(
-      "\nssh -o PreferredAuthentications=password lab@%s",
-      concat(packet_device.lab_alt.*.access_public_ipv4, packet_device.lab.*.access_public_ipv4)
+    join("", formatlist("\n    ssh -o PreferredAuthentications=password lab@%s #password: %s",
+      concat(packet_device.lab_alt.*.access_public_ipv4, packet_device.lab.*.access_public_ipv4),
+      concat(packet_device.lab_alt.*.id, packet_device.lab.*.id)
   ))}"
 }
 
-output "Ansible-Inventory" {
-  value = "${
-    join("", formatlist(
-      "\n%s ansible_host=%s ansible_user=root",
-      concat(packet_device.lab_alt.*.hostname, packet_device.lab.*.hostname),
-      concat(packet_device.lab_alt.*.access_public_ipv4, packet_device.lab.*.access_public_ipv4)
-  ))}"
+output "ansible_inventory" {
+  value = <<EOT
+.
+    [lab]
+${join("\n",
+  formatlist("    %s ansible_host=%s ansible_user=lab",
+    concat(packet_device.lab_alt.*.hostname, packet_device.lab.*.hostname),
+    concat(packet_device.lab_alt.*.access_public_ipv4, packet_device.lab.*.access_public_ipv4)
+))}
+EOT
 }
