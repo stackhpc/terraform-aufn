dd981f223ace90edab23a06840ff3ac65ac9448b
Pierre Riteau <pierre@stackhpc.com>
2020-04-20 16:27:16 +0200

    Fix some Terraform deprecation warnings

--- packet-device-alt.tf
+++ packet-device-alt.tf
@@ -1,19 +1,19 @@
 resource "packet_device" "registry_alt" {
-  depends_on = ["packet_ssh_key.default"]
+  depends_on = [packet_ssh_key.default]
 
   hostname         = "${var.deploy_prefix}-registry-alt"
-  operating_system = "${var.operating_system}"
-  plan             = "${var.plan}"
+  operating_system = var.operating_system
+  plan             = var.plan
 
   connection {
     user        = "root"
-    private_key = "${tls_private_key.default.private_key_pem}"
+    private_key = tls_private_key.default.private_key_pem
     agent       = false
     timeout     = "30s"
     host        = self.access_public_ipv4
   }
-  facilities    = ["${var.packet_facility_alt}"]
-  project_id    = "${var.packet_project_id}"
+  facilities    = [var.packet_facility_alt]
+  project_id    = var.packet_project_id
   billing_cycle = "hourly"
 
   provisioner "file" {
@@ -30,23 +30,23 @@ resource "packet_device" "registry_alt" {
 
 resource "packet_device" "lab_alt" {
 
-  depends_on = ["packet_ssh_key.default"]
+  depends_on = [packet_ssh_key.default]
 
-  count            = "${var.lab_count_alt}"
-  hostname         = "${format("%s-lab-alt-%02d", var.deploy_prefix, count.index)}"
-  operating_system = "${var.operating_system}"
-  plan             = "${var.plan}"
+  count            = var.lab_count_alt
+  hostname         = format("%s-lab-alt-%02d", var.deploy_prefix, count.index)
+  operating_system = var.operating_system
+  plan             = var.plan
 
   connection {
     user        = "root"
-    private_key = "${tls_private_key.default.private_key_pem}"
+    private_key = tls_private_key.default.private_key_pem
     agent       = false
     timeout     = "30s"
     host        = self.access_public_ipv4
   }
 
-  facilities    = ["${var.packet_facility_alt}"]
-  project_id    = "${var.packet_project_id}"
+  facilities    = [var.packet_facility_alt]
+  project_id    = var.packet_project_id
   billing_cycle = "hourly"
 
   provisioner "remote-exec" {
