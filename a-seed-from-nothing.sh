7ab5cabb6a12fd75510918e29ebdf9ee8167a699
Pierre Riteau <pierre@stackhpc.com>
2020-04-20 16:42:11 +0200

    Update scripts for the Train release

--- a-seed-from-nothing.sh
+++ a-seed-from-nothing.sh
@@ -11,18 +11,18 @@ registry_ip=$1
 echo "[INFO] Given docker registry IP: $registry_ip"
 
 # Clone Kayobe.
-git clone https://git.openstack.org/openstack/kayobe.git -b stable/rocky
+[[ -d kayobe ]] || git clone https://git.openstack.org/openstack/kayobe.git -b stable/train
 cd kayobe
 
 # Clone this Kayobe configuration.
 mkdir -p config/src
 cd config/src/
-git clone https://github.com/stackhpc/a-universe-from-nothing.git kayobe-config
+[[ -d kayobe-config ]] || git clone https://github.com/stackhpc/a-universe-from-nothing.git -b stable/train kayobe-config
 
 # Set default registry name to the one we just created
 sed -i.bak 's/^docker_registry.*/docker_registry: '$registry_ip':4000/' kayobe-config/etc/kayobe/docker.yml
 
-# Configure local networking
+# Configure host networking (bridge, routes & firewall)
 ./kayobe-config/configure-local-networking.sh
 
 # Install kayobe.
@@ -35,21 +35,22 @@ cd ~/kayobe
 # Deploy a seed VM.
 # NOTE: This will work the first time because the packet configuration uses a
 # custom docker registry.
-./dev/seed-deploy.sh
+if ! ./dev/seed-deploy.sh; then
+    # Pull, retag images, then push to our local registry.
+    ./config/src/kayobe-config/pull-retag-push-images.sh train
 
-# Pull, retag images, then push to our local registry.
-# ./config/src/kayobe-config/pull-retag-push-images.sh
-
-# Deploy a seed VM. Should work this time.
-# ./dev/seed-deploy.sh
-
-# FIXME: There is an issue with Bifrost which does not restrict the version
-# of proliantutils it installs.
-ssh stack@192.168.33.5 sudo docker exec bifrost_deploy pip install proliantutils==2.7.0
-ssh stack@192.168.33.5 sudo docker exec bifrost_deploy systemctl restart ironic-conductor
+    # Deploy a seed VM. Should work this time.
+    ./dev/seed-deploy.sh
+fi
 
 # Clone the Tenks repository.
-git clone https://git.openstack.org/openstack/tenks.git
+[[ -d tenks ]] || git clone https://git.openstack.org/openstack/tenks.git -b stable/1.0
+
+# Install Open vSwitch for Tenks.
+sudo yum install -y centos-release-openstack-train
+sudo yum install -y openvswitch
+sudo systemctl enable openvswitch
+sudo systemctl start openvswitch
 
 # Duration
 duration=$SECONDS
