75bdcc89556c42fec6df0e9e80ade3a418d2537b
Pierre Riteau <pierre@stackhpc.com>
2020-10-12 15:32:03 +0200

    Fix deprecated syntax

--- output.tf
+++ output.tf
@@ -1,27 +1,26 @@
 output "registry_ip" {
-  value = "${var.lab_count > 0 ? packet_device.registry[0].access_public_ipv4 : ""}"
+  value = var.lab_count > 0 ? packet_device.registry[0].access_public_ipv4 : ""
 }
 
 output "registry_ip_alt" {
-  value = "${var.lab_count_alt > 0 ? packet_device.registry_alt[0].access_public_ipv4 : ""}"
+  value = var.lab_count_alt > 0 ? packet_device.registry_alt[0].access_public_ipv4 : ""
 }
 
 output "lab_ips" {
-  value = "${
-    join("", formatlist("\n    ssh -o PreferredAuthentications=password lab@%s #password: %s",
+  value = join("", formatlist("\n    ssh -o PreferredAuthentications=password lab@%s #password: %s",
       concat(packet_device.lab_alt.*.access_public_ipv4, packet_device.lab.*.access_public_ipv4),
       concat(packet_device.lab_alt.*.id, packet_device.lab.*.id)
-  ))}"
+  ))
 }
 
 output "ansible_inventory" {
   value = <<EOT
 .
     [lab]
-${join("\n",
+join("\n",
   formatlist("    %s ansible_host=%s ansible_user=lab",
     concat(packet_device.lab_alt.*.hostname, packet_device.lab.*.hostname),
     concat(packet_device.lab_alt.*.access_public_ipv4, packet_device.lab.*.access_public_ipv4)
-))}
+))
 EOT
 }
