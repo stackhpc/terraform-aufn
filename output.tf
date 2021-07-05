66349109e326a1b01a3a235ff251ead15e6dc3f0
Bharat Kunwar <brtknr@bath.edu>
2021-07-05 15:16:37 +0100

    Format jumphost correctly

--- output.tf
+++ output.tf
@@ -3,7 +3,8 @@ output "registry_ip" {
 }
 
 output "lab_ips" {
-  value = join("", formatlist("\n    ssh -J centos@${openstack_compute_instance_v2.bastion.access_ip_v6} -o PreferredAuthentications=password lab@%s #password: %s",
+  value = join("", formatlist("\n    ssh -o ProxyCommand='ssh -W %%h:%%p centos@%s' lab@%s #password: %s",
+    trim(openstack_compute_instance_v2.bastion.access_ip_v6, "[]"),
     openstack_compute_instance_v2.lab.*.access_ip_v4,
   openstack_compute_instance_v2.lab.*.id))
 }
