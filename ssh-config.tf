3804160543dbd2cc2ed8bd98ee6f83da4d40856c
Bharat Kunwar <brtknr@bath.edu>
2021-08-17 11:20:59 +0100

    Use IPv4 floating IP instead of IPv6 for bastion

--- ssh-config.tf
+++ ssh-config.tf
@@ -1,6 +1,6 @@
 locals {
   template = {
-    bastion  = tomap({ ip = trim(openstack_compute_instance_v2.bastion.access_ip_v4, "[]"), name = openstack_compute_instance_v2.bastion.name })
+    bastion  = tomap({ ip = openstack_compute_floatingip_associate_v2.bastion.floating_ip, name = openstack_compute_instance_v2.bastion.name })
     registry = tomap({ ip = openstack_compute_instance_v2.registry.access_ip_v4, name = openstack_compute_instance_v2.registry.name })
     labs     = tomap({ names = openstack_compute_instance_v2.lab.*.name, ips = openstack_compute_instance_v2.lab.*.access_ip_v4 })
   }
