6e5759249ee25e145c0081c33667115bf279f2f9
Stig Telfer <stig@stackhpc.com>
2023-04-17 09:17:19 +0100

    Fix locations for Kayobe configuration patching
    Adding Kayboe config to kolla/globals.yml wasn't having the intended effect.

--- a-seed-from-nothing.sh
+++ a-seed-from-nothing.sh
@@ -109,10 +109,14 @@ then
 neutron_plugin_agent: "ovn"
 neutron_ovn_dhcp_agent: "yes"
 EOF
-    cat <<EOF | sudo tee -a config/src/kayobe-config/etc/kayobe/kolla/globals.yml
+    cat <<EOF | sudo tee -a config/src/kayobe-config/etc/kayobe/bifrost.yml
 kolla_bifrost_extra_kernel_options:
   - "console=ttyS0"
+EOF
+    cat <<EOF | sudo tee -a config/src/kayobe-config/etc/kayobe/kolla.yml
 kolla_enable_ovn: yes
+EOF
+    cat <<EOF | sudo tee -a config/src/kayobe-config/etc/kayobe/neutron.yml
 kolla_neutron_ml2_type_drivers:
   - geneve
   - vlan
