ae6e97a00c1df514d22842cc36be035a607b3f9f
Matt Anson <matta@stackhpc.com>
2025-10-16 18:48:24 +0100

    Remove deprecated resource types

--- ssh-config.tf
+++ ssh-config.tf
@@ -1,6 +1,6 @@
 locals {
   template = {
-    bastion  = var.create_bastion ? tomap({ ip = openstack_compute_floatingip_associate_v2.bastion[0].floating_ip, name = openstack_compute_instance_v2.bastion[0].name }) : ({ip="",name=""})
+    bastion  = var.create_bastion ? tomap({ ip = openstack_networking_floatingip_associate_v2.bastion[0].floating_ip, name = openstack_compute_instance_v2.bastion[0].name }) : ({ip="",name=""})
     registry = tomap({ ip = openstack_compute_instance_v2.registry.access_ip_v4, name = openstack_compute_instance_v2.registry.name })
     labs     = tomap({ names = openstack_compute_instance_v2.lab.*.name, ips = openstack_compute_instance_v2.lab.*.access_ip_v4 })
   }
